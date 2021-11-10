2f0fe8b579f60844a9296fe96efe547b8aa20717
KathyLi <lkx626@gmail.com>
2021-11-10 08:37:09 -0600

    Finished all six tasks
    Finished all six tasks, but still need to give it final run to fix some potential small issues

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -4,13 +4,20 @@
 
 /* Implementation of class "MessageQueue" */
 
-/* 
+
 template <typename T>
 T MessageQueue<T>::receive()
 {
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
+     std::unique_lock<std::mutex> lock(_mutex);
+    // wait for message
+    _condition.wait(lock);
+    // pull message from queue
+    T recieved = std::move(_queue.back());
+    _queue.pop_back();
+    return recieved;
 }
 
 template <typename T>
@@ -18,12 +25,16 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> lock(_mutex);
+    _queue.clear();  
+    _queue.push_back(msg);
+    _condition.notify_one();
 }
-*/
+
 
 /* Implementation of class "TrafficLight" */
 
-/* 
+ 
 TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
@@ -34,6 +45,14 @@ void TrafficLight::waitForGreen()
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
+    while(true) {
+        if(_queue.receive() == TrafficLightPhase::green) {
+            return;
+        }
+      // sleep for 1ms to reduce load on the processor
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+    return;
 }
 
 TrafficLightPhase TrafficLight::getCurrentPhase()
@@ -44,6 +63,8 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 void TrafficLight::simulate()
 {
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
+
 }
 
 // virtual function which is executed in a thread
@@ -53,6 +74,40 @@ void TrafficLight::cycleThroughPhases()
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+    
+    //below random generation are referred to
+    std::random_device rd;  
+    std::mt19937 gen(rd()); 
+    std::uniform_int_distribution<> distrib(4, 6); 
+    double cycleDur = distrib(gen);
+
+    // timer
+    std::chrono::time_point<std::chrono::system_clock> time_1;
+    std::chrono::time_point<std::chrono::system_clock> time_2;
+    time_1 = std::chrono::system_clock::now();
+
+    while(true) {
+      // sleep for 1ms to reduce load on the processor
+      std::this_thread::sleep_for(std::chrono::milliseconds(1));
+      
+      //Initialize timer 2
+      time_2 = std::chrono::system_clock::now();
+      
+      // Toggle lights if time duration is greater than above defined cycle duration
+      if ((std::chrono::duration_cast<std::chrono::seconds>(time_2 - time_1).count()) >= cycleDur) {
+        if (_currentPhase == red) 
+        {
+            _currentPhase = green;
+        }
+        else 
+        {
+            _currentPhase = red;
+        }
+        time_1 = std::chrono::system_clock::now();
+        // push each new TrafficLightPhase into Messagequeue using move semantics in conjunction with send
+        _queue.send(std::move(_currentPhase));
+        
+      }
+    }
 }
 
-*/
